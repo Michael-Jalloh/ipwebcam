960a6f6e184d9980afd1bb29bdee141c99247194
Michael-Jalloh <michaeljalloh19@gmail.com>
2017-07-01 03:41:12 +0000

    Fix README.md and added a test file

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-This is a simple script that helps in accessing the image from the Android App
-IP Webcam and to be able to display it in
-pygame
-
-
-# Requirements
-1. **opencv 2**
-2. **numpy**
-3. **pygame**
-
-# Usage
-
-```
-python test.py
-```
-
-```python
 import sys
 import pygame
 from IPWebCam import *
@@ -36,7 +19,3 @@ while run:
   screen.blit(ipcam.get_pygame_image(),(0,0))
   pygame.display.flip()
   clock.tick(0)
-```
-
-# TODO
-Add support for Image resolution
