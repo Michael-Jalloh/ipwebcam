4671be52a6c1fcdba3cee731de8c5c1c8b72e822
Michael-Jalloh <michaeljalloh19@gmail.com>
2020-07-17 19:29:11 +0000

    setup clean up

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setup(
         "requests"
     ],
     classifiers= [
-        "Development Status :: 3 - Alpha",      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        "Intended Audience :: Developers",      # Define that your audience are developers
+        "Development Status :: 3 - Alpha",      
+        "Intended Audience :: Developers",      
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",   # Again, pick a license
-        "Programming Language :: Python :: 3",      #Specify which pyhton versions that you want to support
+        "License :: OSI Approved :: MIT License",   
+        "Programming Language :: Python :: 3",   
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
